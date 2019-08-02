2f2775867feed8d2bf8e10e03c5ccfbc6219c65c
Bohdan Yevtukh <bevtuh@gmail.com>
2019-08-02 14:05:59 +0300

    Add OSR USB device enumerating COM object, it is one and only externally creatable via CoCreateInstance class
    Client code should create an internal device list by calling
    EnumerateDevices, then it is possible to handle any specific devices via
    GetDeviceByIndex. For safety the GetDeviceCount function should be
    called to check the number of supported devices currently plugged in.

---
+++ OsrUsbApi/OsrUsbDeviceEnumerator.h
@@ -0,0 +1,80 @@
+/*++
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Copyright (c) 2019 Bohdan Yevtukh. All rights reserved.
+
+Module Name:
+
+    OsrUsbDeviceEnumerator.h
+
+Abstract:
+
+    This module contains the type definitions for the COsrUsbDeviceEnumerator
+    class.
+
+--*/
+
+#if !defined(OSRUSBDEVICEENUMERATOR_H__2622B424_6F44_4CC8_AB63_EB1B1C93AD31__INCLUDED_)
+#define OSRUSBDEVICEENUMERATOR_H__2622B424_6F44_4CC8_AB63_EB1B1C93AD31__INCLUDED_
+
+//
+// We do not include the resource.h here as it is already included in the
+// precompiled header in order to prevent redefinition warnings
+//
+
+#include <atlbase.h>
+#include <atlcom.h>
+#include <OsrUsbApiDll.h>
+
+//
+// This class provides methods for enumerating IOsrUsbDevice objects
+//
+
+class ATL_NO_VTABLE COsrUsbDeviceEnumerator :
+    public ATL::CComObjectRootEx<ATL::CComMultiThreadModel>,
+    public ATL::CComCoClass<COsrUsbDeviceEnumerator, &CLSID_OsrUsbDeviceEnumerator>,
+    public IOsrUsbDeviceEnumerator
+{
+public:
+    COsrUsbDeviceEnumerator() throw();
+
+DECLARE_REGISTRY_RESOURCEID(IDR_OSRUSBAPI_ENUMERATOR_REGISTRY)
+DECLARE_NOT_AGGREGATABLE(COsrUsbDeviceEnumerator)
+
+BEGIN_COM_MAP(COsrUsbDeviceEnumerator)
+    COM_INTERFACE_ENTRY(IOsrUsbDeviceEnumerator)
+END_COM_MAP()
+
+//
+// IOsrUsbDeviceEnumerator implementation
+//
+public:
+    IFACEMETHOD(EnumerateDevices)();
+    IFACEMETHOD(GetDeviceCount)(__out DWORD* pdwDeviceCount);
+    IFACEMETHOD(GetDeviceByIndex)(__in DWORD dwDeviceId, __out void** ppDevice);
+
+protected:
+    ~COsrUsbDeviceEnumerator() throw();
+
+private:
+//
+// Private to avoid accidental use
+//
+    COsrUsbDeviceEnumerator(const COsrUsbDeviceEnumerator&) throw();
+    COsrUsbDeviceEnumerator& operator=(const COsrUsbDeviceEnumerator&) throw();
+};
+
+//
+// Provide support for the registration, initialization, and creation of a
+// class so it can be externally creatable via CoCreateInstance
+//
+OBJECT_ENTRY_AUTO(__uuidof(OsrUsbDeviceEnumerator), COsrUsbDeviceEnumerator)
+
+#endif /* !defined(OSRUSBDEVICEENUMERATOR_H__2622B424_6F44_4CC8_AB63_EB1B1C93AD31__INCLUDED_) */
